0ac3c84ab6e100c9eecabb56a66613e750aff8ee
Hazel233 <71447085+Hazel233@users.noreply.github.com>
2020-12-10 21:02:45 +0800

    Add files via upload

--- tests/testthat/test-strsplit.R
+++ tests/testthat/test-strsplit.R
@@ -1,6 +1,7 @@
-x <- c("3D/MON&SUN2D/MON&SUN")
 test_that('Strsplit test!', {
+  x <- c("3D/MON&SUN2D/MON&SUN")
   expect_error(strsplit(strsplit(x, "S", type = "WILLBEERROR")))
   expect_equal(strsplit(x, "/", type = "remove"), list(c("3D","MON&SUN2D","MON&SUN")))
   expect_equal(strsplit(x,""),list(c("3", "D", "/", "M", "O", "N", "&", "S", "U", "N","2", "D", "/", "M", "O", "N", "&", "S", "U", "N")))
 })
+
